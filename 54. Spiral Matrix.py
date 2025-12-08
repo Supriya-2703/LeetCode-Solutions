af7436b0e1084b4c5fae3e69476a8e1d046d42e6
Supriya-2703 <supriyabhade03@gmail.com>
2025-12-08 10:25:32 +0530

    Add files via upload

---
+++ 54. Spiral Matrix.py
@@ -0,0 +1,31 @@
+class Solution:
+    def spiralOrder(self, matrix):
+        res = []
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
+
+        while left <= right and top <= bottom:
+
+            # Traverse from left to right
+            for col in range(left, right + 1):
+                res.append(matrix[top][col])
+            top += 1
+
+            # Traverse from top to bottom
+            for row in range(top, bottom + 1):
+                res.append(matrix[row][right])
+            right -= 1
+
+            if top <= bottom:
+                # Traverse from right to left
+                for col in range(right, left - 1, -1):
+                    res.append(matrix[bottom][col])
+                bottom -= 1
+
+            if left <= right:
+                # Traverse from bottom to top
+                for row in range(bottom, top - 1, -1):
+                    res.append(matrix[row][left])
+                left += 1
+
+        return res
