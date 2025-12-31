6a3203442f6f5ccdfca1465b1850e2c33014242e
Supriya-2703 <supriyabhade03@gmail.com>
2025-12-31 16:48:30 +0530

    Add files via upload

---
+++ 60. Permutation Sequence.py
@@ -0,0 +1,15 @@
+import math
+
+class Solution:
+    def getPermutation(self, n, k):
+        numbers = list(range(1, n+1))
+        k -= 1 
+        result = []
+        
+        for i in range(n, 0, -1):
+            fact = math.factorial(i-1)
+            index = k // fact
+            result.append(str(numbers.pop(index)))
+            k %= fact
+        
+        return ''.join(result)
