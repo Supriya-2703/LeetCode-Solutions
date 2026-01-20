8daaa31ff18b0598d0bfc4e2cc32c5c899dcbb79
Supriya-2703 <supriyabhade03@gmail.com>
2026-01-20 09:06:52 +0530

    Add files via upload

---
+++ 80. Remove Duplicates From Sorted Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        if len(nums) <= 2:
+            return len(nums)
+        
+        write = 2  # first two elements are always allowed
+        
+        for i in range(2, len(nums)):
+            if nums[i] != nums[write - 2]:
+                nums[write] = nums[i]
+                write += 1
+        
+        return write
